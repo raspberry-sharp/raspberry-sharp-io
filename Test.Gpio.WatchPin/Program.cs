473e5887775692cc64e1b06006f2918301886a75
keneo <bartek@szabat.com>
2013-09-28 15:30:30 +0200

    labels etc

--- Test.Gpio.WatchPin/Program.cs
+++ Test.Gpio.WatchPin/Program.cs
@@ -11,7 +11,7 @@ namespace Test.Gpio.WatchPin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WatchPin Sample: Try to run it in parallel with i.e. Test.Gpio.HCSR04");
+            Console.WriteLine("WatchPin Sample: Try to run it in parallel with i.e. Test.Gpio.HCSR501 or Test.Gpio.HCSR04");
             Console.WriteLine();
 
             if (args.Length!=1)
@@ -32,6 +32,7 @@ namespace Test.Gpio.WatchPin
 
             Console.WriteLine("\tWatching Pin: {0}", userPin);
             Console.WriteLine();
+            Console.WriteLine("Press CTRL-C to stop");
 
             var procPin = userPin.ToProcessor();
 
@@ -41,15 +42,16 @@ namespace Test.Gpio.WatchPin
             {
                 driver.Allocate(procPin, PinDirection.Input);
 
-                bool val;
+                bool isHigh = driver.Read(procPin);
 
                 while (true)
                 {
-                    val = driver.Read(procPin);
+                    DateTime now = DateTime.Now;
 
-                    Console.WriteLine(DateTime.Now + ": " + val);
+                    Console.WriteLine(now + "." + now.Millisecond.ToString("000") + ": " + (isHigh?"HIGH":"LOW"));
 
-                    driver.Wait(procPin, waitForUp: !val, timeout: 60000);
+                    driver.Wait(procPin, waitForUp: !isHigh, timeout: 7*24*60*60*1000); // 1 week - //todo infinite
+                    isHigh = !isHigh;
                 }
 
                 //driver.Allocate(procPin, PinDirection.Output);
@@ -59,13 +61,17 @@ namespace Test.Gpio.WatchPin
             } finally
             {
                 //driver.Release(procPin); //not sure if its a good idea
+
+                // lets leave it without releasing so that other processes can keep reading from this pind
+
+                
             }
         }
 
         private static void PrintUsage()
         {
-            Console.WriteLine("Usage: Test.Gpio.WatchPin [pin]"); //todo allow watch multiple pins
-            Console.WriteLine("//todo allow watch multiple pins");
+            Console.WriteLine("Usage: Test.Gpio.WatchPin [pin]"); //todo allow watch multiple pins simultanously
+            Console.WriteLine("//todo allow watch multiple pins simultanously");
             Console.WriteLine("Available pins:");
             Enum.GetNames(typeof(ConnectorPin)).ToList().ForEach(Console.WriteLine);
             Console.WriteLine("//todo allow to specify pin in diffrent ways, ie, by name, by wiringPi, etc"); //todo allow to specify pin in diffrent ways, ie, by name, by wiringPi, etc
