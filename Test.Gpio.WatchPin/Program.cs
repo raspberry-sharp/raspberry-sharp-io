dca89fcc24d360bf1f8a711fdaf511526d7f50a4
Eric Bézine <gitub.commit@raspberry-sharp.org>
2013-10-01 22:19:00 +0200

    Merged from keneo-master

--- Test.Gpio.WatchPin/Program.cs
+++ Test.Gpio.WatchPin/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Raspberry.IO.GeneralPurpose;
 
 namespace Test.Gpio.WatchPin
@@ -22,6 +19,8 @@ namespace Test.Gpio.WatchPin
 
             var pinname = args[0];
 
+            //TODO: Allow watch multiple pins simultaneously
+            //TODO: Allow to specify pin in different ways, ie, by name, by wiringPi, etc.
             ConnectorPin userPin;
             if (!Enum.TryParse(pinname, true, out userPin))
             {
@@ -36,45 +35,39 @@ namespace Test.Gpio.WatchPin
 
             var procPin = userPin.ToProcessor();
 
-            IGpioConnectionDriver driver = GpioConnectionSettings.DefaultDriver;
+            var driver = GpioConnectionSettings.DefaultDriver;
 
             try
             {
                 driver.Allocate(procPin, PinDirection.Input);
 
-                bool isHigh = driver.Read(procPin);
+                var isHigh = driver.Read(procPin);
 
                 while (true)
                 {
-                    DateTime now = DateTime.Now;
+                    var now = DateTime.Now;
 
                     Console.WriteLine(now + "." + now.Millisecond.ToString("000") + ": " + (isHigh?"HIGH":"LOW"));
 
-                    driver.Wait(procPin, waitForUp: !isHigh, timeout: 7*24*60*60*1000); // 1 week - //todo infinite
+                    driver.Wait(procPin, !isHigh, 7*24*60*60*1000); // 1 week - //todo infinite
                     isHigh = !isHigh;
                 }
-
-                //driver.Allocate(procPin, PinDirection.Output);
-                //driver.Write(procPin, false);
-
-                //driver.Allocate(procPin, PinDirection.Input);
-            } finally
+            } 
+            finally
             {
-                //driver.Release(procPin); //not sure if its a good idea
-
-                // lets leave it without releasing so that other processes can keep reading from this pind
-
-                
+                // Leaves the pin unreleased so that other processes can keep reading
+                //driver.Release(procPin);
             }
         }
 
         private static void PrintUsage()
         {
-            Console.WriteLine("Usage: Test.Gpio.WatchPin [pin]"); //todo allow watch multiple pins simultanously
-            Console.WriteLine("//todo allow watch multiple pins simultanously");
+            Console.WriteLine("Usage: Test.Gpio.WatchPin [pin]");
             Console.WriteLine("Available pins:");
             Enum.GetNames(typeof(ConnectorPin)).ToList().ForEach(Console.WriteLine);
-            Console.WriteLine("//todo allow to specify pin in diffrent ways, ie, by name, by wiringPi, etc"); //todo allow to specify pin in diffrent ways, ie, by name, by wiringPi, etc
+
+            Console.WriteLine("//todo allow watch multiple pins simultanously");
+            Console.WriteLine("//todo allow to specify pin in diffrent ways, ie, by name, by wiringPi, etc");
             Console.WriteLine("I.e.: sudo mono Test.Gpio.WatchPin.exe P1Pin23");
             Console.WriteLine();
         }
